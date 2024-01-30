1b731722667a160e062e8210fd50984c2192dd43
vudinhsoai <158160849+vudinhsoai@users.noreply.github.com>
2024-01-30 18:52:56 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace buoi_6_cautrulap2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            //for(i=0;i<10;i++)
+            //{
+            //    if(i%2== 0)
+            //    {
+            //        continue;
+            //    }
+            //    Console.WriteLine("So hien tai:"+i);
+            //}
+            //break: thoát khỏi vòng lặp 
+            //for (i=0;i<10; i++)
+            //{
+            //    if(i>5)
+            //    {
+            //        break;
+            //    }
+            //    Console.WriteLine("So hien tai:" + i); 
+            //}
+            //Console.WriteLine("Vong lap dung lai:" + i);
+            // while( điều kiện lăp);
+            //while (i < 10 ) 
+            // {
+            //     Console.WriteLine("So hien tai:" + i); i = *2;
+            //     //i=i*2(câu lênh 2 bước nhảy)
+            // }
+            // Console.WriteLine(" Vong lap dung lai tai:" + i);
+            //int chieuDai = 0;
+            //int chieuRONG = 0;
+            //Console.WriteLine("Chieu dai:");
+            //chieuDai=int.Parse(Console.ReadLine());
+            //Console.WriteLine("Chieu rong:");
+            //chieuRONG=int.Parse(Console.ReadLine());
+            //int bienLapCR = 0;
+            //while (bienLapCR < chieuRong)
+            //{
+            //    // Vẽ ra 1 dòng  *****
+            //    int bienLapCD = 0;
+            //    while (bienLapCD < chieuDai)
+            //    {
+            //        Console.Write("*");
+            //        bienLapCD++;
+            //    }
+            //    Console.WriteLine();
+            //    bienLapCR++;
+            //}
+
+            //int n = int.Parse(Console.ReadLine());
+            //if (n <= 1)
+            //{
+            //    Console.WriteLine(n + " khong phai nguyen to");
+            //}
+            //else
+            //{
+            //    int bienDemUoc = 0;
+            //    // 1 2 3 4 5 6 7 .. n
+            //    for (int i = 1; i <= n; i++)
+            //    {
+            //        if (n % i == 0)
+            //        {
+            //            bienDemUoc++;
+            //        }
+            //    }
+
+            //    if (bienDemUoc == 2)
+            //    {
+            //        Console.WriteLine(n + " la nguyen to");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(n + " khong la nguyen to");
+            //    }
+            //}
+            Console.ReadKey();
+
+        }
+    }
+}
